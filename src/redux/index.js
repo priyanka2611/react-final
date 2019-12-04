9e3e5b42f28581ba34e479bb817a17754acc9be6
ayush jain <ayush.jain@daffodilsw.com>
2019-12-04 12:07:22 +0530

    initial project setup

---
+++ src/redux/index.js
@@ -0,0 +1,17 @@
+import { createStore } from 'redux';
+import makeRootReducer from './reducers';
+
+const store = createStore(makeRootReducer);
+
+export default store;
+
+function isMegaPrimeNumber(number="") {
+    let isMegaPrime = false;
+    for(let index=0; index < number.length; index++){
+        
+    }
+}
+
+function isPrimeNumber(number=""){
+
+}
---
+++ src/redux/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux';
+//Here we will import our reducers
+
+const reducers = {
+    //here we will place our reducers name imported above
+}
+
+const appReducer = combineReducers(reducers);
+
+export default appReducer;
